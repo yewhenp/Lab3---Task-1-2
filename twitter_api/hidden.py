dc24237b60230db2a7af9f97286724e199562be2
Yex000 <y.pankevych@ucu.edu.ua>
2020-02-26 11:38:10 +0200

    normalise keys

--- twitter_api/hidden.py
+++ twitter_api/hidden.py
@@ -4,7 +4,7 @@
 # Create new App and get the four strings
 
 def oauth():
-    return {"consumer_key": "AFF90sjXSGdYZayGxu0AxzNAh",
-            "consumer_secret": "jOPCxdDfG6XrC5Sqw1WTnFd2vCYySjEtC44tMT1A3PXZCK8IPA",
-            "token_key": "1230094692789968896-ZeKtVFHelvGyLvVt75m6fqBZILz8q8",
-            "token_secret": "01jx0fJ0ubdJYT3suzjbB3cYTtO0CCsI6tHfGgWzXBmQL"}
+    return {"consumer_key": "your consumer_key here",
+            "consumer_secret": "your consumer_secret here",
+            "token_key": "your token_key here",
+            "token_secret": "your token_secret here"}
